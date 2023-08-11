308ace3501a4c0e47d343767a80973108f6d935f
martinbhans1 <130181771+martinbhans1@users.noreply.github.com>
2023-08-12 00:51:20 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -11,16 +11,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		// This ensures relative paths are used, needed for GitHub Pages
+		adapter: adapter(),
 		paths: {
-			base: '',
-			assets: ''
+			base: dev ? '' : process.env.BASE_PATH
 		}
 	}
 }
