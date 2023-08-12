9366a87e457fe5bc69b0dd13819de85e81975a7c
martinbhans1 <130181771+martinbhans1@users.noreply.github.com>
2023-08-12 21:21:18 +0200

    fixess

--- svelte.config.js
+++ svelte.config.js
@@ -11,15 +11,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: adapter(),
 		// This ensures relative paths are used, needed for GitHub Pages
 		paths: {
-			base: '',
+			base: process.env.NODE_ENV === 'production' ? '/sveltememorygame.github.io' : '',
 			assets: ''
 		}
 	}
