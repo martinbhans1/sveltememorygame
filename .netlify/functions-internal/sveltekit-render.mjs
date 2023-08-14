c14f44a2f6c0bab549c5ad20c5e54906719918d4
martinbhans1 <130181771+martinbhans1@users.noreply.github.com>
2023-08-14 13:50:37 +0200

    updates

--- .netlify/functions-internal/sveltekit-render.mjs
+++ .netlify/functions-internal/sveltekit-render.mjs
@@ -12,7 +12,7 @@ return {
 	assets: new Set([".nojekyll","favicon.png","images/bjarnis.jpg","images/blippi.jpg","images/carl.jpg","images/fantus.jpg","images/gecko.jpg","images/jj.jpg","images/leo.jpg","images/meekah.jpg","images/mrmonkey.jpg","images/teletubbies.jpg"]),
 	mimeTypes: {".png":"image/png",".jpg":"image/jpeg"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.1539ea0c.js","app":"_app/immutable/entry/app.3dd4c6a3.js","imports":["_app/immutable/entry/start.1539ea0c.js","_app/immutable/chunks/scheduler.e9b79cf6.js","_app/immutable/chunks/singletons.690dbc47.js","_app/immutable/entry/app.3dd4c6a3.js","_app/immutable/chunks/scheduler.e9b79cf6.js","_app/immutable/chunks/index.55879b81.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.ffafc49a.js","app":"_app/immutable/entry/app.2327850b.js","imports":["_app/immutable/entry/start.ffafc49a.js","_app/immutable/chunks/scheduler.e9b79cf6.js","_app/immutable/chunks/singletons.90b43804.js","_app/immutable/entry/app.2327850b.js","_app/immutable/chunks/scheduler.e9b79cf6.js","_app/immutable/chunks/index.55879b81.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../server/nodes/0.js')),
 			__memo(() => import('../server/nodes/1.js')),
--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.91c55c59.js","_app/immutable/chunks/scheduler.e9b79cf6.js","_app/immutable/chunks/index.55879b81.js","_app/immutable/chunks/singletons.690dbc47.js"];
+export const imports = ["_app/immutable/nodes/1.84494575.js","_app/immutable/chunks/scheduler.e9b79cf6.js","_app/immutable/chunks/index.55879b81.js","_app/immutable/chunks/singletons.90b43804.js"];
 export const stylesheets = [];
 export const fonts = [];
